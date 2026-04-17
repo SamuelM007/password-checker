67fe59815a4a9d8a4ce58f38800f78dfee66ad36
SamuelM007 <174657033+SamuelM007@users.noreply.github.com>
2026-04-17 13:04:06 -0700

    merge

--- src/PasswordCheckerTest.java
+++ src/PasswordCheckerTest.java
@@ -5,13 +5,13 @@ import org.junit.jupiter.api.Test;
 public class PasswordCheckerTest {
 
   @Test
-  public void testIfStringInputIsTheSameLengthMedium() {
-    // Arrange
-    PasswordChecker counter = new PasswordChecker(6, 12);
+  public void describePasswordLength_PasswordIs6Long_LengthMedium() {
+    // Arrange 
+    PasswordChecker counter = new PasswordChecker(6,12);
     // Act
-    String actual = counter.describePasswordLength("1234567");
+    String actual = counter.describePasswordLength("123456");
     // Assert
-    assertEquals("lng", actual);
+    assertEquals("medium",actual);
   }
 
 }
